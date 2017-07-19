b21af96b0385c125dcd00eadf7a70251e06929d1
Jamie Waters <jwaters@crimsonhexagon.com>
2017-07-19 16:47:18 -0400

    changed image for mobile

--- src/components/AboutUs/aboutUs.js
+++ src/components/AboutUs/aboutUs.js
@@ -1,25 +1,34 @@
 import React from 'react';
-
-import styles from './aboutUs.scss'
+import Carousel from 'nuka-carousel';
+import styles from './aboutUs.scss';
+import Flamingos from '../../../img/flamingos.png';
+import Xmas from '../../../img/xmas.png';
+import Engagement from '../../../img/engagement.png';
+import FloridaSunset from '../../../img/floridaSunset.png';
+import Florida from '../../../img/florida.png';
 
 class AboutUs extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      image: [
+        Flamingos,
+        Xmas,
+        Engagement
+      ]
+    };
     }
 
   render() {
     return(
-      <div className="aboutUsContainer">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis blandit velit. Aenean lacinia at libero eget posuere. Ut elementum metus vel ullamcorper ultricies. Vivamus eu erat tellus. Maecenas id dapibus libero. Maecenas id justo volutpat, sagittis nisl non, tempus risus. Cras id hendrerit nunc. Vivamus et ullamcorper felis. Nam ac nisi dictum, vehicula enim eget, porta nibh. Pellentesque tristique augue eget quam imperdiet, in pretium odio gravida. In blandit eu turpis vehicula placerat. Praesent sed augue at est porta blandit. Integer lacinia placerat erat ut posuere. Nunc pharetra urna nec venenatis condimentum.
-
-In libero nisl, efficitur ut eleifend eget, placerat et urna. Nunc pretium maximus quam, vitae consectetur dolor sollicitudin nec. Fusce cursus felis nisl, eu vehicula urna feugiat sit amet. Cras commodo imperdiet nisl, non commodo urna elementum nec. Proin semper sapien leo, at egestas enim sodales in. Nunc malesuada, magna molestie congue interdum, velit elit mollis lacus, nec sollicitudin lectus erat vel orci. Sed magna tellus, aliquam eget ex nec, ultricies porta lacus. Mauris eget arcu ut lectus tincidunt ultricies non eget quam. Nullam at elementum dui. Etiam ut fermentum est. Cras euismod erat quis ligula porttitor, sit amet commodo ligula elementum.
-
-Etiam ut feugiat dui, sit amet vestibulum urna. Ut fermentum consectetur malesuada. Nulla commodo tempor dui, non sollicitudin odio elementum eu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla tincidunt, metus eu venenatis pulvinar, odio odio laoreet nulla, et dignissim magna dui fermentum nisi. Aliquam sit amet vestibulum velit, vel lobortis neque. Vestibulum id elementum tortor, sed interdum quam. Duis luctus elit eget fermentum tempus.
-
-Integer sollicitudin sed est ac posuere. Cras tincidunt mi turpis, in imperdiet orci tincidunt vitae. Duis consectetur tincidunt leo, eget vehicula eros dapibus non. Vivamus egestas tincidunt lectus, sed suscipit turpis ultricies sed. Ut sem metus, facilisis nec gravida vel, porta et diam. Morbi ornare sapien ut tellus blandit consequat. Integer nunc urna, sodales eu interdum et, bibendum vitae orci. Phasellus fermentum malesuada nibh, eu fermentum felis consequat non. Suspendisse nunc nisl, dictum sed neque nec, aliquam molestie leo. Mauris gravida diam venenatis eros mattis rutrum. Donec id mi at ex auctor consectetur. Phasellus et iaculis ex. Quisque vulputate tincidunt nunc non aliquam. Nunc ut commodo neque.
-      </p>
+      <div className='carousel'>
+        <Carousel width='100%' swiping={true} autoplay={false} autoplayInterval={4000} dragging={true}  edgeEasing="easeOutCirc" easing="easeInOutLinear" wrapAround={true}>
+            <img src={FloridaSunset} />
+            <img src={Flamingos} />
+            <img src={Florida} />
+            
+        </Carousel>
       </div>
 
       )
