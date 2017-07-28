9943c127a18849510f00bb9796fd8bc86c2c60b3
Jamie Waters <jwaters627@gmail.com>
2017-07-28 15:31:19 -0400

    added some images

--- src/components/Person/person.js
+++ src/components/Person/person.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import SlideShow from '../SlideShow/slideShow';
 import styles from './person.scss';
+import Cancel from 'react-material-icons/icons/navigation/cancel';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 
 class WeddingParty extends React.Component {
 
+ static childContextTypes =
+    {
+        muiTheme: React.PropTypes.object
+    }
+
+    getChildContext()
+    {
+        return {
+            muiTheme: getMuiTheme({
+                palette:{
+                   
+                }
+            })
+        }
+    }
   constructor(props) {
     super(props);
     this.state = {
@@ -19,7 +37,7 @@ class WeddingParty extends React.Component {
     return(
       <div className='fullContainer'>
         <div className="personContainerFull">
-          <p className='closeButton' onClick={this.props.close}>X</p>
+          <Cancel style={{'float':'right', 'width':'34px', 'height':'34px', 'cursor':'pointer'}} onClick={this.props.close}/>
           <h2 className='personNameFull'>{this.props.person.name}</h2>
           <SlideShow image={this.props.person.images} />
           <p className='personDescriptionFull'>{this.props.person.description}</p>
